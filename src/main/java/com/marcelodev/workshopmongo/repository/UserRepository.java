d2d80b6414e9239934436f23d43a40983bf7dd10
marceloalbuquerque-dev <marceloradeveloper@gmail.com>
2021-04-15 19:27:02 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/marcelodev/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/marcelodev/workshopmongo/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.marcelodev.workshopmongo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,10 @@ import com.marcelodev.workshopmongo.domain.User;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String>{
+
+	User save(Optional<User> newObj);
+
+	//User findOne(String id);
 
 	//void delete(String id);
 
